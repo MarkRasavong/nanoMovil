09424b2addf0585351b06c96a803e51d58d6e55e
Mark Rasavong <mark.rasavong@gmail.com>
2021-11-29 19:14:55 +0100

    basic user schema and connecting DB to server

--- server/index.js
+++ server/index.js
@@ -12,5 +12,14 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Server is running!');
+    res.send('Server is online!');
 });
+
+const DB_CONNECTION_URL = process.env.MONGO_DB_URL;
+const PORT = 5000;
+
+mongoose.connect(DB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch(err => console.log(`Server: ${err}`));
+
+mongoose.set('useFindAndModify', false);
